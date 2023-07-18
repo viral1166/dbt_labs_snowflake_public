739cdd21ab1948759f6e1f87e3e6fa2a6371eee4
Viral Patel <agother06@gmail.com>
2023-07-18 14:35:53 +0000

    Adding ref function in DBT models

--- models/VP/stg_employee.sql
+++ models/VP/stg_employee.sql
@@ -1,10 +1,6 @@
-{{ config (
-    materialized="table"
-)}}
-
 with employee_sal as 
 (
 select emp_name, sum(salary) as total_sal from PC_DBT_DB.PUBLIC.EMPLOYEES_SAMPLE group by 1
 )
 
-select * from employee_sal where total_sal>=100000
+select * from employee_sal
