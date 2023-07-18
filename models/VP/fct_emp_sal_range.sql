5d905301024c34d51affc40a54dc4259cbbe3040
Viral Patel <agother06@gmail.com>
2023-07-18 17:09:22 +0000

    NA

--- models/VP/fct_emp_sal_range.sql
+++ models/VP/fct_emp_sal_range.sql
@@ -8,7 +8,6 @@ select * from {{ ref ('stg_employee') }}
 dbt_emp_sal_dim as (
 select * from {{ ref ('stg_sal_range') }}
 )
-
-select distinct  a.emp_name, a.total_sal , case when a.total_sal >=b.min_salary and a.total_sal <=b.max_salary then b.sal_bracket  else 'Undefined' end  
+select distinct  a.emp_name, a.total_sal , case when a.total_sal >=b.min_salary and a.total_sal <=b.max_salary then b.sal_bracket  else 'Undefined' end  as sal_range
 from dbt_emp_sal a , dbt_emp_sal_dim b 
 where 1=1
